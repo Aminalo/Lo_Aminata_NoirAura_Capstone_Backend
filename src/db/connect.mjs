bd65987a18c69169a64d2372e01c44041905bb0e
Amina <107080579+Aminalo@users.noreply.github.com>
2025-10-15 12:19:17 -0400

    add basic /api/salons route placeholder to unblock server

---
+++ src/db/connect.mjs
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectionStr = process.env.mongoURI || "";
+
+async function connectDB() {
+  try {
+    await mongoose.connect(connectionStr);
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error("DB Connection Error:", err.message);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
